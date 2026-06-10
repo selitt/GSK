b1d3ac82d6989136b35c899762ff0b80f0f656c6
selitt <zabirokhin.seva@yandex.ru>
2026-06-10 21:04:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs, make_moons
-
 
 class Gradient:
     def __init__(self, P1, P2):
@@ -14,51 +11,35 @@ class Gradient:
         self.beta = None
         self.u = None
 
-    def fit_strict_gsk(self, max_iter=1000, tol=1e-6):
+    def fit(self, max_iter=1000, tol=1e-6):
         u = np.zeros(self.m)
         u[:self.s] = 1.0 / self.s
         u[self.s:] = 1.0 / self.m_minus_s
-
         w = np.sum(self.P1 * u[:self.s, np.newaxis], axis=0) - np.sum(self.P2 * u[self.s:, np.newaxis], axis=0)
-
         for k in range(max_iter):
-            proj1 = np.dot(self.P1, w)
-            proj2 = np.dot(self.P2, w)
-            i_prime = np.argmin(proj1)
-            i_double_prime = np.argmax(proj2)
+            wp1 = np.dot(self.P1, w)
+            wp2 = np.dot(self.P2, w)
+            i_first = np.argmin(wp1)
+            i_second = np.argmax(wp2)
+            p_first = self.P1[i_first]
+            p_second = self.P2[i_second]
+            delta_u = np.dot(w, w) - np.dot(w, p_first - p_second)
+            if delta_u < tol: break
 
-            p_prime = self.P1[i_prime]
-            p_double_prime = self.P2[i_double_prime]
-
-            w_sq = np.dot(w, w)
-            delta = w_sq - np.dot(w, p_prime - p_double_prime)
-
-            if delta < tol:
-                break
-
-            dir_w = w - p_prime + p_double_prime
-            dir_w_sq = np.dot(dir_w, dir_w)
-
-            if dir_w_sq < 1e-12:
-                break
-
-            lambda_tilde = delta / dir_w_sq
-            lambda_k = min(lambda_tilde, 1.0)
+            a = w - p_first + p_second
+            a_sq = np.dot(a, a)
+            if a_sq < 1e-12: break
+            lambda_k = min(delta_u / a_sq, 1.0)
 
             u = (1 - lambda_k) * u
-            u[i_prime] += lambda_k
-            u[self.s + i_double_prime] += lambda_k
+            u[i_first] += lambda_k
+            u[self.s + i_second] += lambda_k
+            w = (1 - lambda_k) * w + lambda_k * (p_first - p_second)
 
-            w = (1 - lambda_k) * w + lambda_k * (p_prime - p_double_prime)
-        w1_star = np.sum(self.P1 * u[:self.s, np.newaxis], axis=0)
-        w2_star = np.sum(self.P2 * u[self.s:, np.newaxis], axis=0)
-
-        w1_sq = np.dot(w1_star, w1_star)
-        w2_sq = np.dot(w2_star, w2_star)
-        beta = 0.5 * (w2_sq - w1_sq)
-
+        w1 = np.sum(self.P1 * u[:self.s, np.newaxis], axis=0)
+        w2 = np.sum(self.P2 * u[self.s:, np.newaxis], axis=0)
+        beta = 0.5 * (np.dot(w2, w2) - np.dot(w1, w1))
         self.w = w
         self.beta = beta
         self.u = u
-
         return self.w, self.beta
