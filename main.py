108494d1da32301ffa3099355b6a262061a91a2f
selitt <zabirokhin.seva@yandex.ru>
2026-06-18 12:10:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,19 +21,16 @@ class commonGSK:
             i_second = np.argmax(np.dot(self.P2, w))
             p_first = self.P1[i_first]
             p_second = self.P2[i_second]
-            delta_u = np.dot(w, w) - np.dot(w, p_first - p_second)
-            if delta_u < tol: break
-
+            delta = np.dot(w, w) - np.dot(w, p_first - p_second)
+            if delta < tol: break
             a = w - p_first + p_second
             a_sq = np.dot(a, a)
             if a_sq < 1e-12: break
-            lambda_k = min(delta_u / a_sq, 1.0)
-
+            lambda_k = min(delta / a_sq, 1.0)
             u = (1 - lambda_k) * u
             u[i_first] += lambda_k
             u[self.s + i_second] += lambda_k
             w = (1 - lambda_k) * w + lambda_k * (p_first - p_second)
-
         w1 = np.sum(self.P1 * u[:self.s, np.newaxis], axis=0)
         w2 = np.sum(self.P2 * u[self.s:, np.newaxis], axis=0)
         beta = 0.5 * (np.dot(w2, w2) - np.dot(w1, w1))
